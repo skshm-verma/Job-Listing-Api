b37a6fa89415203749161df2a31e1d42c2a783c3
Saksham <skshm.090898@gmail.com>
2024-06-27 16:48:54 +0530

    updated in vercel.json

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Configure CORS
-app.use(cors({
-    origin: ["https://frontend-job-listing.vercel.app"],
-    methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
-    credentials: true
-}));
+// // Configure CORS
+// app.use(cors({
+//     origin: ["https://frontend-job-listing.vercel.app"],
+//     methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
+//     credentials: true
+// }));
+app.use(cors());
 app.use(express.json())
 
 app.use('/user',userRoute);
